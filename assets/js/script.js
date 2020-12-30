c01b5ce8e6f4ab5f29150715755d0401808280d3
sruss07 <srussell1903@yahoo.co.uk>
2020-12-30 14:53:09 +0000

    js for carousel and navbar added

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      let hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
+  $(".nav-link").on("click", function(e){
+    $("a.nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
+ 
+ $(".img-container:gt(0)").hide();
+  setInterval(function () {
+    $(".img-container:first")
+      .fadeOut(5000)
+      .next()
+      .fadeIn(5000)
+      .end()
+      .appendTo("#photo-carousel-gallery");
+  }, 7000);
+});
+
