f9bdecf790c7e80c68f4348c3bd8aca3eee643c0
sruss07 <srussell1903@yahoo.co.uk>
2021-01-02 19:39:48 +0000

    changes to html, css and js for location buttons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,3 +1,28 @@
+// Function to HIDE/SHOW country info with buttons
+const locationButton = document.getElementsByClassName("location-button");
+for (let i = 0; i < locationButton.length; i++) {
+  locationButton[i].addEventListener("click", handleLocationSelect);
+}
+function handleLocationSelect(e) {
+  const locationBtn = e.target.dataset.location;
+  const locationSelection = document.getElementsByClassName("location-section");
+  for (let i = 0; i < locationSelection.length; i++) {
+    const sectionID = locationSelection[i].id;
+    if (sectionID === locationBtn) {
+      locationSelection[i].classList.remove("hidden");
+    } else {
+      locationSelection[i].classList.add("hidden");
+    }
+  }
+  handleLocationReset("location-0");
+  // Click button to show map-country based on the innerText
+  changeMapLocation(e.target.innerText);
+}
+
+
+
+
+
 // Function to HIDE/SHOW Location info with buttons
 const locationButton = document.getElementsByClassName("location-button");
 for (let i = 0; i < locationButton.length; i++) {
