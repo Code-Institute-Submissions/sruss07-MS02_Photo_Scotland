716df10a2aeed8e579e98fba7434ba6e31a72a68
sruss07 <srussell1903@yahoo.co.uk>
2021-01-03 11:26:36 +0000

    added comments for each function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,43 @@
+// Function for smooth scrolling between sections
 $(document).ready(function () {
-  $("a").on("click", function (event) {
+    $("a").on("click", function (event) {
     if (this.hash !== "") {
-      event.preventDefault();
-      let hash = this.hash;
-      $("html, body").animate(
+        event.preventDefault();
+        let hash = this.hash;
+        $("html, body").animate(
         {
-          scrollTop: $(hash).offset().top,
+            scrollTop: $(hash).offset().top,
         },
         800,
         function () {
-          window.location.hash = hash;
+            window.location.hash = hash;
         }
-      );
+        );
     }
-  });
+    });
 
-  $(".nav-link").on("click", function(e){
+// Function to add or remove active class on nav-items
+    $(".nav-link").on("click", function(e){
     $("a.nav-link").removeClass("active");
     $(this).addClass("active");
-  });
- 
- $(".img-container:gt(0)").hide();
-  setInterval(function () {
+    });
+
+// Function for image carousel slideshow 
+    $(".img-container:gt(0)").hide();
+    setInterval(function () {
     $(".img-container:first")
-      .fadeOut(5000)
-      .next()
-      .fadeIn(5000)
-      .end()
-      .appendTo("#photo-carousel-gallery");
-  }, 7000);
+        .fadeOut(5000)
+        .next()
+        .fadeIn(5000)
+        .end()
+        .appendTo("#photo-carousel-gallery");
+    }, 7000);
 });
 
-
+// Function to close mobile menu after click
 window.onclick = function (e) {
-  let dropDown = document.getElementById("navbarResponsive");
-  if (dropDown.classList.contains("show")) {
+    let dropDown = document.getElementById("navbarResponsive");
+    if (dropDown.classList.contains("show")) {
     dropDown.classList.remove("show");
-  }
+    }
 };
